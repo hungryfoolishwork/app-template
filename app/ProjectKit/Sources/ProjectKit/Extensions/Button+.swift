9c8f18e83f3376153aa7cd9fa86259c2012e7bec
Nathan Borror <nathan@nathanborror.com>
2026-08-27 10:03:31 -0700

    Make done and cancel buttons hide icons on macOS

--- app/ProjectKit/Sources/ProjectKit/Extensions/Button+.swift
+++ app/ProjectKit/Sources/ProjectKit/Extensions/Button+.swift
@@ -2,12 +2,18 @@ import SwiftUI
 
 extension Button where Label == SwiftUI.Label<Text, Image> {
 
-    public static func done(_ title: String = "Done", _ action: @escaping () -> Void) -> Button {
+    public static func done(_ title: String = "Done", _ action: @escaping () -> Void) -> some View {
         Button(title, systemImage: "checkmark", action: action)
+            #if os(macOS)
+            .labelStyle(.titleOnly)
+            #endif
     }
 
-    public static func cancel(_ title: String = "Cancel", _ action: @escaping () -> Void) -> Button {
+    public static func cancel(_ title: String = "Cancel", _ action: @escaping () -> Void) -> some View {
         Button(title, systemImage: "xmark", action: action)
+            #if os(macOS)
+            .labelStyle(.titleOnly)
+            #endif
     }
 
     public static func copy(_ title: String = "Copy", _ action: @escaping () -> Void) -> Button {
